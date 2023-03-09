e23eb88b40acff9ed5538f2884dc41724b5a1df4
tacheometry <39647014+tacheometry@users.noreply.github.com>
2023-03-09 04:34:16 +0200

    Fix Dropdown type

--- src/Dropdown.d.ts
+++ src/Dropdown.d.ts
@@ -1,13 +1,18 @@
-import { Element } from "@rbxts/roact"
+import { Element } from "@rbxts/roact";
 
-type IDropdownProps<T> = {
-    Disabled?: boolean;
-    OnItemActivated: (item: T) => void;
+type IDropdownProps<T extends string> = {
     Items: T[];
+	SelectedItem: string;
+    OnItemSelected: (item: T) => void;
+    Disabled?: boolean;
     MaxVisibleRows?: number;
     RowHeightTop?: number;
     RowHeightItem?: number;
     Width?: UDim;
-} & Partial<WritableProperties<Frame>>
+	Position?: UDim2;
+	AnchorPoint?: Vector2;
+	LayoutOrder?: number;
+	ZIndex?: number;
+}
 
-export const Dropdown: <T>(props: IDropdownProps<T>) => Element
+export const Dropdown: <T extends string>(props: IDropdownProps<T>) => Element
